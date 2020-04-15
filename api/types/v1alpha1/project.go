3b640abee792503a400be9d0cb14e92847be64bb
Martin Helmich <m.helmich@mittwald.de>
2020-04-15 23:18:47 +0200

    Migrate to Go modules; bring everything up-to-date

--- api/types/v1alpha1/project.go
+++ api/types/v1alpha1/project.go
@@ -1,11 +1,14 @@
 package v1alpha1
 
+//go:generate controller-gen object paths=$GOFILE
+
 import metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 type ProjectSpec struct {
 	Replicas int `json:"replicas"`
 }
 
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 type Project struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
@@ -13,6 +16,7 @@ type Project struct {
 	Spec ProjectSpec `json:"spec"`
 }
 
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 type ProjectList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata,omitempty"`
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/martin-helmich/kubernetes-crd-example
+
+go 1.13
+
+require (
+	k8s.io/apimachinery v0.17.0
+	k8s.io/client-go v0.17.0
+)
